d6ad4cb53869dcbcd1fd11acc4ff42f2616f8a6e
[HassanDayoub] <[hassan.dayoub1994@gmail.com]>
2020-02-08 14:40:46 +0200

    add ml code

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.registry import MLRegistry
+import inspect
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +17,21 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+# ML Registry
+
+try:
+    registry = MLRegistry()
+    # Random Forest Classifier
+    rf = RandomForestClassifier()
+    # add to ML Registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                           algorithm_object=rf,
+                           algorithm_name="random forest",
+                           algorithm_status="production",
+                           algorithm_version="0.0.1",
+                           owner="Piotr",
+                           algorithm_description="Random Forest with simple pre- and post-processing",
+                           algorithm_code=inspect.getsource(RandomForestClassifier))
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
